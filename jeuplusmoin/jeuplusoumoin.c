7d75d4a7e90ec5924792fecf5c8480c57cb2e63d
Marco RODRIGUES <marcoa_rodrigues@hotmail.com>
2018-09-10 15:06:08 +0200

    1  commit jeu plus ou moins

---
+++ jeuplusmoin/jeuplusoumoin.c
@@ -0,0 +1,62 @@
+/*
+ ============================================================================
+ Name        : jeuplusoumoin.c
+ Author      : marco
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	 int nombreMystere = 0, nombreEntre = 0;
+
+	    const int MAX = 10, MIN = 1;
+
+
+	    // Génération du nombre aléatoire
+
+
+	    srand(time(NULL));
+
+	    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+
+
+	    /* La boucle du programme. Elle se répète tant que l'utilisateur n'a pas trouvé le nombre mystère */
+
+
+	    do
+
+	    {
+
+	        // On demande le nombre
+
+	        printf("Quel est le nombre ? ");
+
+	        scanf("%d", &nombreEntre);
+
+
+	        // On compare le nombre entré avec le nombre mystère
+
+
+	        if (nombreMystere > nombreEntre)
+
+	            printf("C'est plus !\n\n");
+
+	        else if (nombreMystere < nombreEntre)
+
+	            printf("C'est moins !\n\n");
+
+	        else
+
+	            printf ("Bravo, vous avez trouve le nombre mystere !!!\n\n");
+
+	    } while (nombreEntre != nombreMystere);
+
+
+
+	    return 0;
+}
